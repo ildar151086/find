ea8911aa454e28be594e89ed81b74fc91c67cba1
ildar151086 <71975751+ildar151086@users.noreply.github.com>
2022-05-20 09:08:01 +0500

    MInMAXTree

--- tree2.cpp
+++ tree2.cpp
@@ -18,7 +18,6 @@ public:
 		root = nullptr;
 	}
 	void insert(Element* leaf) {
-		//cout << leaf->value << " ";
 		leaf->left  = nullptr;
 		leaf->right = nullptr;
 		Element* node = root;
@@ -47,17 +46,19 @@ public:
 	void print(Element* leaf) {
 		if (leaf != nullptr) {
 			print(leaf->left);
-			cout << leaf->value << endl;
+			cout << leaf->value << " ";
 			print(leaf->right);
 		}
 	}
 
-	bool find(int valueFind) {
+	// Поиск узла по значению элемента
+	Element* findNode(int valueFind) {
 		Element* node = root;
+
 		while (node != nullptr) {
 			if (valueFind == node->value) {
-				cout << "Число нашли " << valueFind;
-				return true;
+				//cout << "Узел нашли " << valueFind << endl;				
+				return node;// Возвращаем адрес узла
 			}
 			if (valueFind < node->value) {
 				node = node->left;
@@ -66,8 +67,54 @@ public:
 				node = node->right;
 			}
 		}
-		cout << "Число не нашли " << valueFind;
-		return false;
+		cout << "Узел не нашли " << valueFind << endl;
+		return nullptr;
+	};
+
+	// Поиск макисмальньо и минимального элемента заданного узла
+	void maxMinNode(Element* bazeNode) {
+		if (bazeNode == nullptr) {
+			cout << "Такого узла не существует" << endl;
+			return;
+		}
+		// Ищим максимальный элемент данного узла. Самая правая ветка до конца
+		Element* temp = bazeNode;
+		int max = temp->value;
+
+		while (temp != nullptr) {
+			if (max < temp->value) {
+				max = temp->value;				
+			}
+			temp = temp->right;
+		}
+		// Ищим максимальный элемент данного узла. Самая правая ветка до конца
+		temp = bazeNode;
+		int min = temp->value;
+
+		while (temp != nullptr) {
+			if (min > temp->value) {
+				min = temp->value;
+			}
+			temp = temp->left;
+		}
+		cout << "Максимальный элемент данного узла:\t" << max << endl;
+		cout << "Минимальный элемент данного узла:\t" << min << endl;
+	}
+
+	// Поиск макисмального и минимального числа по всему дереву независимо от входного узла
+	void maxMinTree(Element* bazeNode) {
+		if (bazeNode == nullptr) {
+			cout << "Такого узла не существует" << endl;
+			return;
+		}
+		// Находим родителя
+		while (bazeNode->parents != nullptr) {
+			if (bazeNode->parents != nullptr) {
+				bazeNode = bazeNode->parents;
+			}
+		}
+		// Заупскаем поиск в главного родителя по всему дереву
+		maxMinNode(bazeNode);
 	}
 };
 
@@ -76,12 +123,13 @@ int main(){
 	srand(time(0));
 	setlocale(LC_ALL, "RUS");
 
-	int const SIZE = 5;
+	int const SIZE = 500;
 	Element element[SIZE];
 
 	for (int i = 0; i < SIZE; i++) {
-		element[i].value = rand() % 30;
-		//cout << element[i].value << " ";
+		int randValue = rand() % 50 + 1;
+		element[i].value = randValue;
+		cout << element[i].value << " ";
 	}
 	cout << endl;
    
@@ -89,10 +137,15 @@ int main(){
 	for (int i = 0; i < SIZE; i++) {
 		tree.insert(&element[i]);
 	}
+	cout << "Отсортированый массив ";
 	tree.print(&element[0]);
 
-	int user = -1;
-	cout << " Что нам найти ";
-	cin >> user;
-	tree.find(user);
+	cout << "\n Введите узел для анализа: ";
+	int userValure;
+	cin >> userValure;
+	Element* findNode = tree.findNode(userValure);	
+	
+	tree.maxMinNode(findNode);
+
+	tree.maxMinTree(findNode);
 }
